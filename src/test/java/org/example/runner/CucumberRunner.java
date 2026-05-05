796777298409b375afbf3b1ee1dd5e769fd197a4
Pinky Denman <pinkydenman@gmail.com>
2026-05-05 11:51:39 +0100

    assignment- add DI instead of context static, cucumber Runner

---
+++ src/test/java/org/example/runner/CucumberRunner.java
@@ -0,0 +1,12 @@
+package org.example.runner;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("features")
+public class CucumberRunner {
+}
